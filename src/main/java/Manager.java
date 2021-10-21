653dd05950376ade6187d02a0a52828a28c51456
FilsanMadar <filsanmadar@gmail.com>
2021-10-21 00:51:49 -0400

    Initial commit

---
+++ src/main/java/Manager.java
@@ -0,0 +1,18 @@
+public class Manager extends Employee {
+
+    protected static double getBasicSalary() {
+        return basicSalary;
+    }
+    protected static void setBasicSalary(double basicSalary) {
+        Manager.basicSalary = basicSalary;
+    }
+
+    @Override
+    public double calculateTransportAllowance () {
+        double transportAllowance = 15 * basicSalary/100;
+        return transportAllowance;
+    }
+
+}
+
+
